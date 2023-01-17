95416821f3b06301b1d226ac94d55abe5aef064e
MeghaShyam31 <122890800+MeghaShyam31@users.noreply.github.com>
2023-01-17 21:06:23 +0530

    IMDb program

---
+++ imdb1-fe/src/pages/home/home.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react"
+import "./home.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from "react-router-dom";
+import MovieList from "../../components/movieList/movieList";
+/* learn usestate, carousel is for automatic changing of movies list and all is return in car. so thaht every thing changes as slides
+learn link tag, link, i tag,skeleton loading,useparams,hover in css, animantion in css*/
+
+const Home = () => {
+
+    const [ popularMovies, setPopularMovies ] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
+        .then(res => res.json())
+        .then(data => setPopularMovies(data.results))
+    }, [])
+
+    return (
+        <>
+            <div className="poster">
+                <Carousel
+                    showThumbs={false}
+                    autoPlay={true}
+                    transitionTime={1}
+                    infiniteLoop={false}
+                    showStatus={false}
+                >
+                    {
+                        popularMovies.map(movie => (
+                            <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
+                                <div className="posterImage">
+                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
+                                </div>/* for movie poster*/
+                                <div className="posterImage__overlay">
+                                    <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
+                                    <div className="posterImage__runtime">
+                                        {movie ? movie.release_date : ""}
+                                        <span className="posterImage__rating">
+                                            {movie ? movie.vote_average :""}
+                                            <i className="fas fa-star" />{" "}
+                                        </span>
+                                    </div>
+                                    <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </Carousel>
+                <MovieList />
+            
+            </div>
+        </>
+    )
+}
+
+export default Home
